ef42b4d3d23a9a485630c1f30d91e8d139466c17
daikun0919 <daikin091964@gmail.com>
2020-09-08 05:51:02 +0900

    initial commit

--- db/migrate/20200902031136_create_user_accounts.rb
+++ db/migrate/20200902031136_create_user_accounts.rb
@@ -2,7 +2,8 @@ class CreateUserAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :user_accounts do |t|
       t.string :card_name, null: false
-      t.string :expiration_date, null: false
+      t.string :card_period, null: false
+      t.string :security_code, null: false
       t.timestamps
     end
   end
