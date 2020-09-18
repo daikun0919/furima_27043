ea144861ad1abf03f6d70fbbd7251610cda41a66
daikun0919 <daikin091964@gmail.com>
2020-09-18 10:21:47 +0900

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,23 +17,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
 end
 
 
-
-# class ApplicationController < ActionController::Base
-#   before_action :basic_auth
-#   (省略)
-
-#   private
-
-#   def basic_auth
-#     authenticate_or_request_with_http_basic do |username, password|
-#       username == 'admin' && password == '2222'
-#     end
-#   end
-# end
 
 
 
