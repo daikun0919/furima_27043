2b3af0d751c5c6ef212433eded011a41ec860446
daikun0919 <daikin091964@gmail.com>
2020-09-19 10:16:06 +0900

    initial commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,12 +15,12 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :image
-    validates :item_price
+    validates :item_price, numericality: { greater_than: 300, less_than: 999999 }
+
+
   end
   
   belongs_to :user
   has_one :order
   has_one_attached :image
 end
-
-# :numericality => { greater_than: 300, less_than: 999,999 }
